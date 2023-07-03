b1914ef4b9ee085d062a9aa45222c5607c2ea8d4
benguimo <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>
2023-07-03 20:56:22 +0000

    .

--- noughts-crosses.js
+++ noughts-crosses.js
@@ -1,11 +1,12 @@
-const message = document.getElementById("players")
-const board = document.getElementById("parent")
-const tieScore = document.getElementById("tie-score")
-const player1Score = document.getElementById("player1-score")
-const player2Score = document.getElementById("player2-score")
 
-let boardState
+const message = document.getElementById("players");
+const board = document.getElementById("parent");
+const tieScore = document.getElementById("tie-score");
+const player1Score = document.getElementById("player1-score");
+const player2Score = document.getElementById("player2-score");
+const gameMode = document.getElementById("game-mode");
 
+let boardState;
 let winStates = [
     [1, 1, 1,
      0, 0, 0,
@@ -18,19 +19,15 @@ let winStates = [
     [0, 0, 1, 0, 0, 1, 0, 0, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 1],
     [0, 0, 1, 0, 1, 0, 1, 0, 0]
-]
+];
 
-let player
-
+let player;
 const playerSymbols = {1: "X", 2: "O"};
-
-let messageText
-
-let gameOver
-
-let nextPlayer  = 1
-
-let scoreUpdate = {1 : 0, 2 : 0, tie: 0}
+let messageText;
+let gameOver;
+let nextPlayer = 1;
+let scoreUpdate = {1: 0, 2: 0, tie: 0};
+let isPlayerVsGame = true
 
 function foundZero(num) {
     if (num === 0) return true;
@@ -43,27 +40,26 @@ function tickBox(square) {
         board.children[square].innerText = playerSymbols[player];
 
         if (checkWinStates(player)) {
-            messageText = `PLAYER ${player} WINS!`
-            scoreUpdate[player]++
-            player1Score.innerText = scoreUpdate[1]
-            player2Score.innerText = scoreUpdate[2]
-            gameOver = true
+            messageText = `PLAYER ${player} WINS!`;
+            scoreUpdate[player]++;
+            player1Score.innerText = scoreUpdate[1];
+            player2Score.innerText = scoreUpdate[2];
+            gameOver = true;
+        } else if (boardState.some(foundZero) === false) {
+            messageText = `TIE!`;
+            scoreUpdate.tie++;
+            tieScore.innerText = scoreUpdate.tie;
+            gameOver = true;
+        } else {
+            if (player === 1) player = 2;
+            else if (player === 2) player = 1;
+            messageText = `Player ${player} turn`;
+            if (isPlayerVsGame && player === 2) {
+                simulateComputerTurn();
+            }
         }
 
-        else if (boardState.some(foundZero) === false) {
-            messageText = `TIE!`
-            scoreUpdate.tie++
-            tieScore.innerText = scoreUpdate.tie
-            gameOver = true
-        }
-
-        else{  
-            if(player === 1) player = 2;
-            else if(player === 2) player = 1;
-            messageText = `Player ${player} turn`
-        }
-
-        message.innerText = messageText
+        message.innerText = messageText;
     }
 }
 
@@ -72,38 +68,57 @@ function checkWinStates(player) {
         let matches = 0;
 
         for (let i in winState) {
-
             if (winState[i] === 1) {
-                if(boardState[i] === player) matches++;
+                if (boardState[i] === player) matches++;
             }
         }
 
         if (matches === 3) {
-            return true
+            return true;
         }
     }
-    return false
+    return false;
 }
-
 
 function reset() {
+    boardState = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ];
 
-     boardState = [0, 0, 0,
-                   0, 0, 0,
-                   0, 0, 0];
+    for (const child of board.children) {
+        child.innerText = "";
+    }
+    gameOver = false;
 
-        for(const child of board.children) {
-            child.innerText = ""
-        }
-        gameOver = false
-
-        player = nextPlayer
-        if(player === 1) nextPlayer = 2;
-        else if(player === 2) nextPlayer = 1;
-        message.innerText = `Player ${player} turn`
+    player = nextPlayer;
+    if (player === 1) nextPlayer = 2;
+    else if (player === 2) nextPlayer = 1;
+    message.innerText = `Player ${player} turn`;
 }
 
+function simulateComputerTurn() {
+    if (!gameOver) {
+        setTimeout(() => {
+            for (let i = 0; i < boardState.length; i++) {
+                if (boardState[i] === 0) {
+                    tickBox(i);
+                    break;
+                }
+            }
+        }, 500);
+    }
+}
 
-reset()
+function toggleGameMode() {
+    isPlayerVsGame = !isPlayerVsGame;
+    if (isPlayerVsGame) {
+        gameMode.innerText = "Player vs Game";
+    } else {
+        gameMode.innerText = "Player vs Player";
+    }
+}
 
+reset();
 
