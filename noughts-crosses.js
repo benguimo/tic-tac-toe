fe0034ad49ee5c6e71de8b8a192aea4ba9ba53fc
benguimo <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>
2023-07-03 17:02:43 +0000

    .

--- noughts-crosses.js
+++ noughts-crosses.js
@@ -1,9 +1,10 @@
 const message = document.getElementById("players")
 const board = document.getElementById("parent")
+const tieScore = document.getElementById("tie-score")
+const player1Score = document.getElementById("player1-score")
+const player2Score = document.getElementById("player2-score")
 
-let boardState = [0, 0, 0,
-                  0, 0, 0,
-                  0, 0, 0];
+let boardState
 
 let winStates = [
     [1, 1, 1, 0, 0, 0, 0, 0, 0],
@@ -16,21 +17,41 @@ let winStates = [
     [0, 0, 1, 0, 1, 0, 1, 0, 0]
 ]
 
-let player = 1;
+let player
 
 const playerSymbols = {1: "X", 2: "O"};
 
+let messageText
+
+let gameOver
+
+let nextPlayer  = 1
+
+let scoreUpdate = {1 : 0, 2 : 0, tie: 0}
+
 function tickBox(i) {
-    if (boardState[i] === 0) {
+    if (!gameOver && boardState[i] === 0) {
         boardState[i] = player;
         board.children[i].innerText = playerSymbols[player];
 
-        checkWinStates(player);
+        if (checkWinStates(player)) {
+            messageText = `PLAYER ${player} WINS!`
+            scoreUpdate[player]++
+            player1Score.innerText = scoreUpdate[1]
+            player2Score.innerText = scoreUpdate[2]
+            gameOver = true
+            }
 
+
+      else{  
         if(player === 1) player = 2;
         else if(player === 2) player = 1;
+        messageText = `Player ${player} turn`
+      }
 
-        message.innerText = `Player ${player} turn`;
+
+        message.innerText = messageText
+      
     }
 }
 
@@ -46,7 +67,43 @@ function checkWinStates(player) {
         }
 
         if (matches === 3) {
-            console.log(`player ${player} wins`);
+            return true
+           /*  console.log(message.innerText) */
         }
+
     }
+    return false
+    
 }
+
+function score() {
+
+}
+
+
+
+
+function reset() {
+    //Able to play again (gameOver = false)
+    //Players to normal
+
+
+     boardState = [0, 0, 0,
+        0, 0, 0,
+        0, 0, 0];
+
+        for(const child of board.children) {
+            child.innerText = ""
+        }
+        gameOver = false
+
+        player = nextPlayer
+        if(player === 1) nextPlayer = 2;
+        else if(player === 2) nextPlayer = 1;
+        message.innerText = `Player ${player} turn`
+}
+
+
+reset()
+
+
